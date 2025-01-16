58decb10b1bbdd9b9023297126f9f4cd76911ed0
Jeongmingz <joungminyt@gmail.com>
2025-01-16 14:13:29 +0900

    feat: Instagram Post Image Download API

--- insta/apis.py
+++ insta/apis.py
@@ -1,10 +1,18 @@
-from instaloader import LoginException
+import json
+
+from django.db import transaction
+from instaloader import LoginException, BadResponseException
+from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from django.contrib.auth.hashers import make_password, check_password
-from insta.models import Insta
+from insta.models import Insta, InstaHistory, InstaFuncType
 from insta.serializers import InstaUserCreateSerializer
-from share.functions import return_login_insta_instance
+from insta.utils import check_insta_login
+from share.functions import return_login_insta_instance, post_shortcode_downloader
 from utils.response import Response
+
+import time
 
 
 class InstaUserApi(APIView):
@@ -17,13 +25,13 @@ class InstaUserApi(APIView):
             return Response(data={'error': 'Missing id or pw'}, status=400)
 
         try:
-            return_login_insta_instance(insta_id, insta_pw)
+            return_login_insta_instance(insta_id, request.data.get('pw', None))
         except LoginException as e:
 	        if 'Checkpoint required' in str(e):
 		        pass
 	        else:
 		        return Response(
-			        data=e.__str__(),
+			        data=str(e),
 			        status=490
 			        )
 
@@ -46,12 +54,92 @@ class InstaUserApi(APIView):
 
             serializer = InstaUserCreateSerializer(data=create_data)
             if serializer.is_valid():
-                serializer.save(create_data)
+                insta =serializer.save(create_data)
             else:
                 return Response(serializer.errors, status=400)
 
-        # 비밀번호 해시 값 생성
+        # Create a CustomResponse object
+        response = Response(status=status.HTTP_204_NO_CONTENT)
 
-        return Response(
-            status=203,
-        )
+        # Set a cookie on the response
+        response.set_cookie(
+            key="insta",
+            value=json.dumps({"id": insta.id, "name": insta_id}),
+            max_age=3600,
+            httponly=True,
+            secure=False,
+            samesite="Lax"
+            )
+
+        return response
+
+
+#
+@api_view(['GET'])
+def insta_image_download_api_view(request):
+	now = time.time()
+	is_login, code = check_insta_login(request)
+	if not is_login:
+		return Response(status=code)
+
+	try:
+		insta = Insta.objects.select_related().get(id=code)
+		func_type = InstaFuncType.objects.get(pk=1)
+	except Insta.DoesNotExist:
+		return Response(status=491)
+	except InstaFuncType.DoesNotExist:
+		return Response(msg="Function type not found", status=500)
+
+	history_instance = InstaHistory(type=func_type, insta=insta)
+
+	try:
+		images = post_shortcode_downloader(request.data.get('url'))
+	except BadResponseException as e:
+		if "Fetching Post metadata failed." == str(e):
+			query = {
+				"request": {
+					'type': InstaFuncType.objects.get(pk=1).type,
+					'url': request.data.get('url'),
+					},
+				"response": {
+					'code': "ERROR",
+					'status': 494,
+					'reason': str(e)
+					}
+				}
+
+			history_instance.query = query
+			history_instance.save()
+
+			return Response(status=494)
+
+		else:
+			return Response(msg=str(e), status=500)
+
+	runtime = time.time() - now
+	formatted_runtime = f"{int(runtime * 1000)} ms" if runtime < 1 else f"{round(runtime, 2)} s"
+
+	response_data = {
+		'images': images,
+		'cnt': len(images),
+		'runtime': formatted_runtime,
+		}
+
+	query = {
+		"request": {
+			'type': func_type.type,
+			'url': request.data.get('url'),
+			},
+		"response": {
+			'code': "SUCCESS",
+			'status': 200,
+			'cnt': len(images),
+			'runtime': formatted_runtime,
+			}
+		}
+
+	with transaction.atomic():
+		history_instance.query = query
+		history_instance.save()
+
+	return Response(data=response_data, status=200)
