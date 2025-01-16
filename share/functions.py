64e0e1f8daa07f0f13d50518196a6b18b66828c5
Jeongmingz <joungminyt@gmail.com>
2025-01-16 14:20:18 +0900

    feat: Instagram Post Image Download 함수

--- share/functions.py
+++ share/functions.py
@@ -1,8 +1,11 @@
+import base64
+from datetime import datetime
 import os
+
 import environ
 from pathlib import Path
 
-from instaloader import Instaloader, LoginException
+from instaloader import Instaloader, instaloader
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -16,7 +19,28 @@ def return_env_value(key: str) -> str:
 
 
 def return_login_insta_instance(id: str, pw: str) -> Instaloader:
-    instance = Instaloader()
+    instance = instaloader.Instaloader()
     instance.login(id, pw)
 
     return instance
+
+def post_shortcode_downloader(url: str) -> list():
+    # Extract shortcode from URL
+    shortcode = url.strip().split('/')[-2]
+    instance = instaloader.Instaloader()
+
+    # Load post using shortcode
+    post = instaloader.Post.from_shortcode(instance.context, shortcode)
+
+    # List to store base64-encoded images
+    base64_encoding_images = []
+
+    for i, image in enumerate(post.get_sidecar_nodes()) :
+        temp_filename = "temp_image"
+        instance.download_pic(temp_filename, image.display_url, datetime.now())
+
+        with open(temp_filename+'.jpg', "rb") as img_file:
+            base64_image = base64.b64encode(img_file.read()).decode("utf-8")
+            base64_encoding_images.append(base64_image)
+
+    return base64_encoding_images
